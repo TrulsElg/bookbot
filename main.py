46cf0a6015595646b41d927152d67711b5e2bc69
TrulsElg <trulseb@gmail.com>
2025-09-15 17:58:27 +0200

    Added function to count letters in content

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 def count_words(content):
     return len(content.split())
 
+def count_letters(content):
+    letter_dict = dict()
+
+    lines = "".join(content.split("\n"))
+    for char in lines:
+        if char.isalpha():
+            c = char.lower()
+            if c in letter_dict.keys():
+                letter_dict[c] += 1
+            else:
+                letter_dict[c] = 1
+
+    return letter_dict
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         
-    print(count_words(file_contents))
+    print(count_letters(file_contents))
 
 if __name__ == "__main__":
     main()
