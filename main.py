02e1248eefa7400659ab70dd3c7f774d26c28599
TrulsElg <trulseb@gmail.com>
2025-09-15 17:58:28 +0200

    Refactor book analysis to helper function

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-from stats import count_words, count_letters, sort_dictionary
+from stats import analyze_book
 
 def main():
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-
-    print("----------- Word Count ----------")
-    word_count = count_words(content=file_contents)
-    print(f"Found {word_count} total words found in the document")
-
-    print("--------- Character Count -------")
-    sorted_letters = sort_dictionary(count_letters(file_contents))
-    for l in sorted_letters:
-        if not l[0].isalpha():
-            continue
-        print(f"{l[0]}: {l[1]}")
-
-    print("============= END ===============")
+    analyze_book("books/frankenstein.txt")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+import json
+
+
 def count_words(content):
     return len(content.split())
 
@@ -16,3 +19,22 @@ def count_letters(content):
 
 def sort_dictionary(dictionary):
     return sorted(dictionary.items(), key=lambda d: d[1], reverse=True)
+
+def analyze_book(book_path):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    with open(book_path) as f:
+        file_contents = f.read()
+
+    print("----------- Word Count ----------")
+    word_count = count_words(content=file_contents)
+    print(f"Found {word_count} total words found in the document")
+
+    print("--------- Character Count -------")
+    sorted_letters = sort_dictionary(count_letters(file_contents))
+    for l in sorted_letters:
+        if not l[0].isalpha():
+            continue
+        print(f"{l[0]}: {l[1]}")
+
+    print("============= END ===============")
