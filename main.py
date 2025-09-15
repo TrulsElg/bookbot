542281ac772087f305937f5ce2955b570c2db4c1
TrulsElg <trulseb@gmail.com>
2025-09-15 17:58:28 +0200

    Parse first argument as path to book file

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
 from stats import analyze_book
 
 def main():
-    analyze_book("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    analyze_book(sys.argv[1])
 
 if __name__ == "__main__":
     main()
